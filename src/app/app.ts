44686889883e977e44d3c7a40b372f3323cbc063
ulicortes <ulisestcortes@gmail.com>
2026-02-08 16:23:56 -0300

    Portafolio creado.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from "./menu/menu";
+import { SobreMi } from "./sobre-mi/sobre-mi";
+import { Proyectos } from "./proyectos/proyectos";
+import { Skills } from "./skills/skills";
+import { Contacto } from "./contacto/contacto";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Menu, SobreMi, Proyectos, Skills, Contacto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
