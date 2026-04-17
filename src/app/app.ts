07a57c65c92c37e42e114d9646f514b31ab91d44
ulicortes <ulisestcortes@gmail.com>
2026-04-17 17:13:24 -0300

    Par de cambios agregados que hacian falta.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Menu } from "./menu/menu";
-import { SobreMi } from "./sobre-mi/sobre-mi";
-import { Proyectos } from "./proyectos/proyectos";
-import { Skills } from "./skills/skills";
-import { Contacto } from "./contacto/contacto";
+import { Menu } from './menu/menu';
+import { SobreMi } from './sobre-mi/sobre-mi';
+import { Proyectos } from './proyectos/proyectos';
+import { Skills } from './skills/skills';
+import { Contacto } from './contacto/contacto';
+import { Experiencia } from './experiencia/experiencia';
 
 @Component({
   selector: 'app-root',
-  imports: [Menu, SobreMi, Proyectos, Skills, Contacto],
+  imports: [Menu, SobreMi, Proyectos, Skills, Contacto, Experiencia],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('portfolio');
---
+++ src/app/experiencia/experiencia.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencia',
+  imports: [],
+  templateUrl: './experiencia.html',
+  styleUrl: './experiencia.css',
+})
+export class Experiencia {
+
+}
